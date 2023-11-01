e813607b282ce91788f605fce6a710a45e942086
James Haywood <jameshaywood@fastmail.com>
2023-10-31 22:36:10 -0400

    Chunk compilation work in build script

--- build/main.rs
+++ build/main.rs
@@ -4,7 +4,10 @@ mod codegen;
 mod dev;
 mod language;
 
-use language::*;
+use std::num::NonZeroUsize;
+use std::thread;
+
+use crate::language::*;
 
 fn main() -> anyhow::Result<()> {
     let languages = std::fs::read_to_string("build/languages.toml")?;
@@ -13,20 +16,32 @@ fn main() -> anyhow::Result<()> {
     #[cfg(feature = "development")]
     dev::check(&languages)?;
 
-    let handles: Vec<_> = languages
+    let languages: Vec<_> = languages
         .into_iter()
         .filter(|lang| {
             let name = lang.name.to_uppercase().replace('-', "_");
             let name = format!("CARGO_FEATURE_LANGUAGE_{name}");
             std::env::var(name).is_ok()
         })
-        .map(|lang| std::thread::spawn(move || {
-            lang.compile()
-        }))
         .collect();
 
-    for handle in handles {
-        handle.join().expect("Compilation thread should not panic")
+    let threads = thread::available_parallelism()
+        .unwrap_or(NonZeroUsize::MIN)
+        .into();
+    
+    for chunk in languages.chunks(threads) {
+        thread::scope(|s| {
+            let handles: Vec<_> = chunk
+                .iter()
+                .map(|lang| s.spawn(|| lang.compile()))
+                .collect();
+
+            for handle in handles {
+                handle
+                    .join()
+                    .expect("Compilation thread should not panic")
+            }
+        })
     }
 
     Ok(())
