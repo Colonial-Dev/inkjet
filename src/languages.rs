529efce2f22b9b3f6bb5de1b97e65aa1d298c0a3
Leandro Pereira <leandro@leandro.io>
2024-04-04 18:04:20 -0400

    Fix `Lua` config (#15)
    * Fix lua config
    
    Query files are located in a subdir
    
    * remove unused files from lua config

--- src/languages.rs
+++ src/languages.rs
@@ -1325,7 +1325,9 @@ pub mod gleam {
     pub const HIGHLIGHT_QUERY: &str = include_str!(
         "../languages/gleam/queries/highlights.scm"
     );
-    pub const INJECTIONS_QUERY: &str = "";
+    pub const INJECTIONS_QUERY: &str = include_str!(
+        "../languages/gleam/queries/injections.scm"
+    );
     pub const LOCALS_QUERY: &str = include_str!("../languages/gleam/queries/locals.scm");
     #[cfg(test)]
     mod tests {
@@ -2070,9 +2072,13 @@ pub mod lua {
         config.configure(HIGHLIGHT_NAMES);
         config
     });
-    pub const HIGHLIGHT_QUERY: &str = "";
-    pub const INJECTIONS_QUERY: &str = "";
-    pub const LOCALS_QUERY: &str = "";
+    pub const HIGHLIGHT_QUERY: &str = include_str!(
+        "../languages/lua/queries/highlights.scm"
+    );
+    pub const INJECTIONS_QUERY: &str = include_str!(
+        "../languages/lua/queries/injections.scm"
+    );
+    pub const LOCALS_QUERY: &str = include_str!("../languages/lua/queries/locals.scm");
     #[cfg(test)]
     mod tests {
         use super::*;
