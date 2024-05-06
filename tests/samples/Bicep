0689f413b02c4cd972fbbf3a1589ef50f5382120
James Haywood <jameshaywood@fastmail.com>
2024-05-06 18:07:30 -0400

    Start sourcing test snippets

---
+++ tests/samples/Bicep
@@ -0,0 +1,4 @@
+param yourName string
+var hello = 'Hello World! - Hi'
+
+output helloWorld string = '${hello} ${yourName}'
--- tests/snippets.rs
+++ tests/snippets.rs
@@ -6,3 +6,17 @@ fn highlight_all_snippets() {
 
     }
 }
+
+#[test]
+fn generate_snippet_files() {
+    use std::path::Path;
+    use std::fs::File;
+
+    for lang in Language::ALL_LANGS {
+        let path = format!("./tests/samples/{lang:?}");
+
+        if !Path::new(&path).exists() {
+            File::create(&path).unwrap();
+        }
+    }
+}
