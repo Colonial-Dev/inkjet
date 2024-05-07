43e566d710b2de4d3344603b1a9f8cf935d4ed60
James Haywood <jameshaywood@fastmail.com>
2024-05-06 21:22:55 -0400

    Implement non-themed snippet testing

--- tests/snippets.rs
+++ tests/snippets.rs
@@ -1,9 +1,37 @@
+use std::fs::File;
+use std::io::Write;
+
 use inkjet::*;
+use inkjet::formatter::Html;
 
 #[test]
 fn highlight_all_snippets() {
-    for lang in Language::ALL_LANGS {
+    let mut out = File::create("./tests/snippets.html")
+        .unwrap();
 
+    let mut hili = Highlighter::new();
+
+    for lang in Language::ALL_LANGS {
+        let source = std::fs::read_to_string(
+            format!("./tests/samples/{lang:?}")
+        ).unwrap();
+        
+        let _ = write!(
+            &mut out,
+            "<h2>{lang:?}</h2><pre>"
+        );
+
+        hili.highlight_to_writer(
+            *lang,
+            &Html,
+            source,
+            &mut out
+        ).unwrap();
+
+        let _ = write!(
+            &mut out,
+            "</pre>"
+        );
     }
 }
 
